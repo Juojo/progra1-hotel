73c20cd07956cd84ad7477797cce5c64201a3ac0
Rodrigo Galarce <rgalarce@uade.edu.ar>
2025-08-12 16:08:13 -0300

    Se agregó el archivo con matrices

---
+++ TPO_hotel.py
@@ -0,0 +1,31 @@
+# Datos predefinidos
+encabezados_clientes = ['Id_clientes', 'Nombre', 'Apellido', 'DNI']
+clientes = [
+    [1, 'Juan', 'Pérez', '43664686'],
+    [2, 'Ana', 'López', '34686546'],
+    [3, 'Carlos', 'Gómez', '39605003'],
+    [4, 'María', 'Fernández', '41040550'],
+    [5, 'Lucía', 'Martínez', '37999439'],
+]
+
+encabezados_habitaciones = ['Nro_habitacion', 'Tipo', 'Capacidad', 'Estado']
+habitaciones = [
+    [1, 'Suite', '2', "Libre"],
+    [2, 'Suite', '2', "Ocupada"],
+    [3, 'Residencial', '3', "Reservada"],
+    [4, 'Presidencial', '5', "Libre"],
+    [5, 'Suite', '2', "Libre"],
+]
+
+encabezados_reservas = ['Id_reserva', 'Cliente', 'Habitacion', 'Fecha_ingreso', 'Fecha_egreso']
+reservas = [
+    [1, 1, 3, '04/07/2025', '12/07/2025'],
+    [2, 2, 1, '28/06/2025', '29/06/2025'],
+    [3, 3, 2, '05/07/2025', '19/07/2025'],
+    [4, 5, 4, '02/11/2025', '06/11/2025'],
+    [5, 4, 5, '27/09/2025', '02/10/2025'],
+]
+
+# Funciones
+
+# Programa Principal
