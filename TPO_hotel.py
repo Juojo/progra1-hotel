73c4c88d21bbd381184153c47ea26687e92a5455
Rodrigo Galarce <rgalarce@uade.edu.ar>
2025-08-17 11:35:30 -0300

    Se agregaron las funciones del menu principal y de los clientes

--- TPO_hotel.py
+++ TPO_hotel.py
@@ -144,20 +144,161 @@ def mostrarIngresarMenuLogin(usuarios):
     print()
     return login_exitoso
 
+# FUNCIONES GENERALES DEL MENU
+
+def mostrarMenuPrincipal():
+        print('''
+--- Sistema de Reservas de Hotel ---
+
+1. Agregar cliente
+2. Modificar cliente
+3. Borrar cliente
+4. Mostrar clientes
+5. Agregar reserva
+6. Cancelar reserva
+7. Modificar reserva
+8. Mostrar todas las reservas
+9. Ver estado de habitaciones
+10. Modificar estado de habitacion
+-------------------------------
+0. Salir
+''')
+        
+def imprimirTituloOpcion(titulo):
+    print()
+    print(f"--- {titulo.upper()} ---")
+    print()
+        
+def mostrarIngresarVolverMenu():
+    # Volver al menu principal
+    print("\nPresione ENTER para volver al menú principal", end="")
+    input()
+
+    mostrarMenuPrincipal()
+
+def mostrarIngresarMenuPrincipal(clientes):
+    mostrarMenuPrincipal()
+    
+    opcion_seleccionada = "" # Se inicializa la variable
+
+    while opcion_seleccionada != "0":
+        opcion_seleccionada = input("Seleccione una opción: ")
+        
+        if opcion_seleccionada == "1":
+            agregarCliente(clientes)
+        elif opcion_seleccionada == "2":
+            modificarCliente(clientes)
+        elif opcion_seleccionada == "3":
+            borrarCliente(clientes)
+        elif opcion_seleccionada == "4":
+            mostrarClientes(clientes)
+        elif opcion_seleccionada == "5":
+            pass
+        elif opcion_seleccionada == "6":
+            pass
+        elif opcion_seleccionada == "7":
+            pass
+        elif opcion_seleccionada == "8":
+            pass
+        elif opcion_seleccionada == "9":
+            pass
+        elif opcion_seleccionada == "10":
+            pass
+        else:
+            if opcion_seleccionada != "0":
+                print()
+                print("Opción no válida.")
+                print()
+
 # Clientes
 
-def agregarCliente():
-    pass
+def agregarCliente(clientes):
+    dni = input("Ingrese el DNI del cliente:")
+    existe = False
+    # Se busca si existe un cliente con ese DNI
+    for i in range(len(clientes)):
+        if clientes[i][3] == dni:
+            existe = True
+    if existe:
+        print("Ya existe un cliente registrado con ese DNI.")
+    else:
+        nuevo_id = clientes[len(clientes)-1][0] + 1
+    
+    nombre = input("Ingrese el nombre del cliente: ")
+    apellido = input("Ingrese el apellido del cliente: ")
+
+    nuevo_cliente = [nuevo_id, nombre, apellido, dni]
+    clientes.append(nuevo_cliente)
+    print("Se agrego el cliente correctamente.")
+    mostrarIngresarVolverMenu()
+
+def borrarCliente(clientes):
+    dni = input("Ingrese el DNI del cliente:")
+    existe = False
+    # Se busca si existe un cliente con ese DNI
+    for i in range(len(clientes)):
+        if clientes[i][3] == dni:
+            existe = True
+            indice = i  # Se guarda la posicion del cliente
+    if existe:
+        clientes.pop(indice)
+        print("Se ha borrado el cliente correctamente.")
+    else:
+        print("Error. No hay un cliente registrado con ese DNI.")
+    mostrarIngresarVolverMenu()
+
+def modificarCliente(clientes):
+    dni = input("Ingrese el DNI del cliente: ")
+    existe = False
+    # Se busca si existe un cliente con ese DNI
+    for i in range(len(clientes)):
+        if clientes[i][3] == dni:
+            existe = True
+            indice = i
+
+    if existe:
+        nuevo_nombre = input("Ingrese el nuevo nombre del cliente: ")
+        nuevo_apellido = input("Ingrese el nuevo apellido del cliente: ")
+        nuevo_dni = input("Ingrese el nuevo DNI del cliente (0 si no quiere modificarlo): ")
+
+        # Se verifica que el nuevo DNI no esté repetido si no lo quiere cambiar
+        if nuevo_dni != "0":  
+            repetido = False
+            for j in range(len(clientes)):
+                if j != indice and clientes[j][3] == nuevo_dni:
+                    repetido = True
+
+            if repetido:
+                print("Error: ya existe otro cliente con ese DNI.")
+                return clientes
+            else:
+                clientes[indice][3] = nuevo_dni
+            
+        clientes[indice][1] = nuevo_nombre
+        clientes[indice][2] = nuevo_apellido  
+        print("Se han modificado los datos del cliente correctamente.")
+    else:
+        print("Error. No hay un cliente registrado con ese DNI.")
+
+    mostrarIngresarVolverMenu()
+    
+
+def mostrarClientes(clientes):
+    print(f"{'ID':<10} {'Nombre':<10} {'Apellido':<10} {'DNI'}")
+    print("-" * 40)
+    for cliente in clientes:
+        print(f"{cliente[0]:<10} {cliente[1]:<10} {cliente[2]:<10} {cliente[3]}")
+    mostrarIngresarVolverMenu()
 
 # Reservas
 
-def agregarReserva():
+def agregarReserva(reservas):
     pass
 
-def modificarReserva(id_reserva):
+def modificarReserva(reservas):
     pass
 
-def darBajaReserva(id_reserva):
+def darBajaReserva(reservas):
     pass
 
 # Programa Principal
@@ -165,6 +306,5 @@ def darBajaReserva(id_reserva):
 login_exitoso = mostrarIngresarMenuLogin(usuarios)
 
 if login_exitoso:
-    print("Inicio programa")
-
+    mostrarIngresarMenuPrincipal(clientes)
 print("Fin programa")
