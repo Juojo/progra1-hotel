1ebfe08942b7f04ed71a4f594a48cebb75ddfd0f
Juojo <80736129+Juojo@users.noreply.github.com>
2025-10-07 16:57:55 -0300

    Agrega user de desarrollo

--- usuarios/datos.py
+++ usuarios/datos.py
@@ -1,4 +1,5 @@
 usuarios = [
 ('Recepcionista_1', '1234', 'recepcionista1@gmail.com'),
-('Recepcionista_2', '5678', 'recepcionista2@gmail.com')
+('Recepcionista_2', '5678', 'recepcionista2@gmail.com'),
+('', '', '')
 ]
